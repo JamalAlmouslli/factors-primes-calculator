37b26826733f9e6fc8453fca727ec34282b28871
Jamal <JamalAlmouslli@users.noreply.github.com>
2016-11-02 13:19:32 -0500

    Update FactorsPrimesCalculator.cpp

--- FactorsPrimesCalculator.cpp
+++ FactorsPrimesCalculator.cpp
@@ -7,107 +7,107 @@ using Primes = std::map<int, int>;
 
 Factors calculateFactors(long long int integer, int& numberOfFactors)
 {
-	Factors factors;
-	
-	// these avoids a call to std::sqrt()
+    Factors factors;
+    
+    // these avoids a call to std::sqrt()
     // doing so also avoids casting
-	long long int incr = 0;
-	long long int incrSum = 1;
-	
-	// start loop based on even or odd integer
-	long long int i = (integer % 2 == 0) ? 2 : 3;
-	
-	for (; incrSum <= integer; i++)
+    long long int incr = 0;
+    long long int incrSum = 1;
+    
+    // start loop based on even or odd integer
+    long long int i = (integer % 2 == 0) ? 2 : 3;
+    
+    for (; incrSum <= integer; i++)
     {
-    	// get remainder and quotient at once
-    	std::lldiv_t result = std::lldiv(integer, i);
-    	
+        // get remainder and quotient at once
+        std::lldiv_t result = std::lldiv(integer, i);
+        
         if (result.rem == 0)
         {
-			factors[i] = result.quot;
-			numberOfFactors += (i == factors[i]) ? 1 : 2;
+            factors[i] = result.quot;
+            numberOfFactors += (i == factors[i]) ? 1 : 2;
         }
 
         // addition is usually faster
-		incr += 2;
-		incrSum += incr;
+        incr += 2;
+        incrSum += incr;
     }
-	
-	return factors;
+    
+    return factors;
 }
 
 Primes calculatePrimes(long long int integer)
 {
-	Primes primes;
-	int divisor = 2;
+    Primes primes;
+    int divisor = 2;
 
-	while (integer % divisor == 0)
-	{
-		integer /= divisor;
-		primes[divisor]++;
-	}
-
-	for (divisor = 3; integer > 1; divisor += 2)
+    while (integer % divisor == 0)
     {
-		while (integer % divisor == 0)
-		{
-			integer /= divisor;
-			primes[divisor]++;
-		}
+        integer /= divisor;
+        primes[divisor]++;
     }
-	
-	return primes;
+
+    for (divisor = 3; integer > 1; divisor += 2)
+    {
+        while (integer % divisor == 0)
+        {
+            integer /= divisor;
+            primes[divisor]++;
+        }
+    }
+    
+    return primes;
 }
 
 std::ostream& operator<<(std::ostream& out, Factors const& factors)
 {
-	for (auto& iter : factors)
-	{
-		out << iter.first << " x " << iter.second << "\n";
-	}
+    for (auto& iter : factors)
+    {
+        out << iter.first << " x " << iter.second << "\n";
+    }
 
-	return out;
+    return out;
 }
 
 std::ostream& operator<<(std::ostream& out, Primes const& primes)
 {
-	const auto firstPrime = primes.cbegin();
-	
-	for (auto& iter : primes)
-	{
-		// don't print 'x' before first prime
-		if (&iter != &*firstPrime)
-		{
-			out << " x ";
-		}
+    const auto firstPrime = primes.cbegin();
+    
+    for (auto& iter : primes)
+    {
+        // don't print 'x' before first prime
+        if (&iter != &*firstPrime)
+        {
+            out << " x ";
+        }
 
-		out << iter.first;
+        out << iter.first;
 
         // don't print exponent if 1
-		if (iter.second > 1)
-		{
-			out << '^' << iter.second;
-		}
-	}
+        if (iter.second > 1)
+        {
+            out << '^' << iter.second;
+        }
+    }
 
-	return out;
+    return out;
 }
 
 int main()
 {
-	long long int integer;
-	std::cin >> integer;
+    long long int integer;
+    std::cin >> integer;
 
     // includes 1 and itself
     // such calculation not needed
-	int numberOfFactors = 2;
-	
-	std::cout << "Factors of " << integer << ":\n\n";
-	Factors factors = calculateFactors(integer, numberOfFactors);
-	std::cout << "1 x " << integer << "\n" << factors;
-	std::cout << "\n(" << numberOfFactors << " unique factors)\n\n";
-	
-	std::cout << "Prime Factorization of " << integer << ":\n\n";
-	Primes primes = calculatePrimes(integer);
-	std::cout << primes;
+    int numberOfFactors = 2;
+    
+    std::cout << "Factors of " << integer << ":\n\n";
+    Factors factors = calculateFactors(integer, numberOfFactors);
+    std::cout << "1 x " << integer << "\n" << factors;
+    std::cout << "\n(" << numberOfFactors << " unique factors)\n\n";
+    
+    std::cout << "Prime Factorization of " << integer << ":\n\n";
+    Primes primes = calculatePrimes(integer);
+    std::cout << primes;
 }
